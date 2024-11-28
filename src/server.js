b35d32f2351e5e009ce1d71250669b95cc1c31e5
Janielson Anjos <jan.tablet.uni@gmail.com>
2024-11-28 01:14:50 -0300

    feat: adicionando cors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import indexRouter from "./routes/index.js";
+import cors from "cors"
 
 const app = express();
 app.use(express.json());
 dotenv.config();
+app.use(cors());
 
 const PORT = process.env.PORT;
 
 app.use("/api", indexRouter);
 
 app.listen(PORT, () => {
-  console.log(`rodando server na url: http://localhost:${PORT}/`);
+  console.log(`rodando server`);
 });
